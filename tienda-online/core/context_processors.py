7ab6f6cbb544072e51abd1bb8a23a19e10723b28
christian1913 <75808879+christian1913@users.noreply.github.com>
2023-06-05 19:35:37 +0200

    count carrito

--- tienda-online/core/context_processors.py
+++ tienda-online/core/context_processors.py
@@ -1,4 +1,24 @@
 from django.contrib.auth.forms import AuthenticationForm
+from frontend.carrito.models import Carrito
+
 
 def login_form(request):
     return { 'login_form': AuthenticationForm() }
+
+def cart_count(request):
+    session_key = request.session.session_key
+    if not session_key:
+        # Si no hay una clave de sesión, forzar la creación de una
+        request.session.create()
+        session_key = request.session.session_key
+
+    if request.user.is_authenticated:
+        carrito, _ = Carrito.objects.get_or_create(usuario=request.user)
+    else:
+        carrito, _ = Carrito.objects.get_or_create(session_key=session_key)
+
+    cart_count = carrito.carrito.count()
+
+    return {
+        'cart_count': cart_count,
+    }
